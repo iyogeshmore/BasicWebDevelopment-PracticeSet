3cea664157eca7aa832c906221b03050f9b45b1f
Yogesh More <iyogesh.more1998@gmail.com>
2022-06-18 20:07:33 +0530

    Added : Sum Of Two Numbers By Receiving Value From User.

--- JS/Sum_UserInput.js
+++ JS/Sum_UserInput.js
@@ -5,3 +5,5 @@ var a , b;
 a = parseInt(prompt("Enter First Number: "));
 b = parseInt(prompt("Enter Second Number: "));
 console.log(" Sum Of Two Numbers: ",a+b);
+
+// Here, prompt() is used to take inputs from the user. parseInt() is used to convert the user input string to number.
